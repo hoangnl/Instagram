f48dd2ab1d01b07e5db66684dd939a34336c6985
hoangnl <nguyenlehoang@hotmail.com>
2016-06-12 14:11:58 +0700

    Unittest message

---
+++ Instagram.Test/Message/MessageControllerTest.cs
@@ -0,0 +1,37 @@
+﻿using Instagram.Controllers;
+using Instagram.Helpers;
+using Instagram.Service.Feed;
+using Instagram.Service.Message;
+using Instagram.ViewModel.Feed;
+using Instagram.ViewModel.User;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Security.Principal;
+using System.Web.Mvc;
+
+namespace Instagram.Test
+{
+    [TestFixture]
+    public class MessageControllerTest
+    {
+        [Test]
+        public void Index_ValidInput_ValidIndexView()
+        {
+            //Arrange
+            IMessageService MessageService = Substitute.For<IMessageService>();
+            IUserHelper UserHelper = Substitute.For<IUserHelper>();
+
+            MessageService.GetPagingMessage(Arg.Any<string>(), Arg.Any<int>(), Arg.Any<int>()).Returns(new MessageWrapperViewModel());
+            UserHelper.GetCurrentUserIdFromClaim(Arg.Any<IPrincipal>()).Returns("hoangnl");
+
+            MessageController controller = new MessageController(MessageService, UserHelper);
+            //Act
+            ViewResult editView = controller.Index() as ViewResult;
+            //Assert
+            Assert.IsEmpty(editView.ViewName);
+            Assert.IsTrue((editView.Model as MessageWrapperViewModel) != null);
+        }
+
+    }
+}
---
+++ Instagram.Test/Message/MessageServiceTest.cs
@@ -0,0 +1,71 @@
+﻿using Instagram.Controllers;
+using Instagram.Helpers;
+using Instagram.Model.Common;
+using Instagram.Model.Repository;
+using Instagram.Model.UnitOfWork;
+using Instagram.Service.Message;
+using Instagram.ViewModel.Feed;
+using NSubstitute;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Security.Principal;
+using System.Web.Mvc;
+
+namespace Instagram.Test
+{
+    [TestFixture]
+    public class MessageSerivceTest
+    {
+        [Test]
+        public void GetPagingMessage_ValidInput_ReturnValidMessageWrapperViewModel()
+        {
+            //Arrange
+            IDatabaseFactory databaseFactory = Substitute.For<IDatabaseFactory>();
+            UnitOfWork unitOfWork = new UnitOfWork(databaseFactory);
+            IMessageRepository messageRepository = Substitute.For<IMessageRepository>();
+            unitOfWork.MessageRepository = messageRepository;
+            messageRepository.GetMany(Arg.Any<Expression<Func<Model.EDM.Message, bool>>>()).Returns(new List<Model.EDM.Message>() {
+                new Model.EDM.Message() { FromUserId = "123", ToUserId ="456" }
+            });
+            IUserRepository userRepository = Substitute.For<IUserRepository>();
+            unitOfWork.UserRepository = userRepository;
+            userRepository.GetWithInclude(Arg.Any<Expression<Func<Model.EDM.User, bool>>>()).Returns(new List<Model.EDM.User> {
+                new Model.EDM.User() { UserId ="123" },
+                new Model.EDM.User() { UserId ="456" }
+            });
+            MessageService messageSerivce = new MessageService(unitOfWork);
+
+            //Act
+            var messageWrapper = messageSerivce.GetPagingMessage("123", 0, 5);
+            var userViewModels = messageWrapper.Users as List<UserViewModel>;
+            //Assert
+            Assert.IsTrue(userViewModels[0].UserId == "123" && userViewModels[1].UserId == "456");
+
+        }
+
+        [Test]
+        public void GetPagingMessage_ValidInput_ReturnEmptyMessageWrapperViewModel()
+        {
+            //Arrange
+            IDatabaseFactory databaseFactory = Substitute.For<IDatabaseFactory>();
+            UnitOfWork unitOfWork = new UnitOfWork(databaseFactory);
+            IMessageRepository messageRepository = Substitute.For<IMessageRepository>();
+            unitOfWork.MessageRepository = messageRepository;
+            messageRepository.GetMany(Arg.Any<Expression<Func<Model.EDM.Message, bool>>>()).Returns(new List<Model.EDM.Message>() {
+                new Model.EDM.Message() { FromUserId = "123", ToUserId ="456" }
+            });
+            IUserRepository userRepository = Substitute.For<IUserRepository>();
+            unitOfWork.UserRepository = userRepository;
+            userRepository.GetWithInclude(Arg.Any<Expression<Func<Model.EDM.User, bool>>>()).Returns(x => null);
+            MessageService messageSerivce = new MessageService(unitOfWork);
+
+            //Act
+            var messageWrapper = messageSerivce.GetPagingMessage("123", 0, 5);
+            //Assert
+            Assert.IsNull(messageWrapper.Users);
+
+        }
+    }
+}
